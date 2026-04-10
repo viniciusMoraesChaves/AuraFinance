97de41f01463eec7c59983e534ffd23df6eae253
Vinicius <viniciusdemoraeschaves@gmail.com>
2026-04-10 15:18:56 -0300

    fix: route to pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 
 import LandingPage from './assets/pages/LandingPage'
 import DashBoard from './assets/pages/DashBoard'
+import Objectives from './assets/pages/Objectives'
+import Savings from './assets/pages/Savings'
+import Transactions from './assets/pages/Transactions'
 
 import { Routes, Route } from 'react-router-dom'
 
@@ -13,6 +16,9 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/objectives" element={<Objectives />} />
+        <Route path="/savings" element={<Savings />} />
+        <Route path="/transactions" element={<Transactions />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
