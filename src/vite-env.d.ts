d4357fe864496c172f9e5f5bfaedac40d32189ed
Vinicius <viniciusdemoraeschaves@gmail.com>
2026-04-10 15:20:46 -0300

    chore: add vite-env type declarations

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+declare module '*.avif';
+declare module '*.bmp';
+declare module '*.gif';
+declare module '*.jpeg';
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.webp';
