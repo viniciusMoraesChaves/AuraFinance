565af5ff25eec5727b3fbc8375439fe1f1630f19
Vinicius <viniciusdemoraeschaves@gmail.com>
2026-04-23 18:10:24 -0300

    feat: hover effect

--- src/assets/components/DasBoardExpensesByCategory.tsx
+++ src/assets/components/DasBoardExpensesByCategory.tsx
@@ -3,6 +3,7 @@ import {
   Pie,
   Cell,
   ResponsiveContainer,
+  Tooltip,
 } from "recharts";
 
 const data = [
@@ -13,6 +14,34 @@ const data = [
 ];
 
 export function DashBoardExpensesByCategory() {
+const CustomTooltip = ({ active, payload }: any) => {
+    if (active && payload && payload.length) {
+      const item = payload[0];
+
+      return (
+        <div
+          style={{
+            background: "#ffffff",
+            border: "1px solid #e5e7eb",
+            borderRadius: "8px",
+            padding: "10px",
+            boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
+            fontSize: "12px",
+          }}
+        >
+          <p style={{ margin: 0, color: "#6b7280" }}>
+            {item.name}
+          </p>
+          <p style={{ margin: 0, fontWeight: 600 }}>
+            R${item.value}
+          </p>
+        </div>
+      );
+    }
+    return null;
+  };
+
+
   return (
     <div
       style={{background: "#fff", borderRadius: "16px", padding: "20px", boxShadow: "0 2px 8px rgba(0,0,0,0.05)", flex: 1,}}>
@@ -28,6 +57,7 @@ export function DashBoardExpensesByCategory() {
       <div style={{ width: "100%", height: "200px" }}>
         <ResponsiveContainer>
           <PieChart>
+            <Tooltip content={<CustomTooltip />} cursor={false} wrapperStyle={{outline: "none",zIndex: 1000,}}/>
             <Pie
               data={data}
               dataKey="value"
